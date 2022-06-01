bc9908fe910c65547d9435150ad17a59dcd198d3
Gui Costa <gui.costalp@gmail.com>
2022-06-01 13:14:26 +1200

    Pseudo code and some parameters created

---
+++ java.js
@@ -0,0 +1,31 @@
+console.log("Hello World")
+
+/*Pseudo code
+    function to decide computer turn
+        can chose between paper, scissors or rock
+        var to storage result
+
+    prompt to ask users choices
+        can chose between paper, scissors or rock
+        var to storage resul
+   
+    function to set choices values
+        i.e rock wins over scissors
+        may need several functions for each possibility
+
+    function to play the round. 
+        choose between computer and user choices
+
+    string with turn result
+        "You Lose! Paper beats Rock"
+
+    Make user choice case-insensitive (rock, ROCK, RocK)
+
+    
+
+
+*/
+
+function computerPlay() {
+
+}

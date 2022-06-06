d404a8e5f6987f13eefa3a84d3439613583c0ab0
Gui Costa <gui.costalp@gmail.com>
2022-06-06 17:49:14 +1200

    Project finished, game runs until someone score 5 wins

--- java.js
+++ java.js
@@ -3,9 +3,12 @@ let playerScore = 0
 let computerScore = 0
 
 
-/**Loop 5x turns**/
-for (let i = 0; i < 5; i++) { 
+/**Loop 100x turns**/
+for (let i = 0; i < 100; i++) { 
 
+/*Run game until one score 5*/
+
+if (playerScore < 5 && computerScore < 5) {
 
 let computerSelection = getRandomChoice()
 let playerSelection = 'ROCK'/*prompt("Choose one").toUpperCase() //**.toUpperCase() used to make all strings Upper case and compere with optons | This make input case insensitive**/
@@ -13,11 +16,22 @@ let playerSelection = 'ROCK'/*prompt("Choose one").toUpperCase() //**.toUpperCas
 
 console.log (`Player Selection: ${playerSelection}`)
 console.log (`Computer Selection: ${computerSelection}`)
-console.log (`playRound: ${playRound (playerSelection, computerSelection)}`)
 
+/**Computer Choice**/
 
+function getRandomChoice () {                       
+    let randomNumber = Math.floor(Math.random() * 3); /*Get 3 random numbers*/
+    switch (randomNumber) {
+        case 0:
+            return 'ROCK'
+        case 1:
+            return 'PAPER'
+        case 2:
+            return 'SCISSORS'
+    }  
+}
 
-/*One turn fuction (for loop runs 5 times)*/
+/*One turn fuction (for loop runs n times)*/
 
 function playRound (playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
@@ -40,39 +54,39 @@ function playRound (playerSelection, computerSelection) {
 
 
 
-/*Score fuction*/
+
+/*Score and Round Result Message*/
 
 let roundResult = playRound(playerSelection, computerSelection)
-console.log (`roundResult: ${roundResult}`)
+
 
 if (roundResult === 'player') {
     playerScore++
+    console.log (`You win! ${playerSelection} beats ${computerSelection}`)
 }
 
 else if (roundResult === 'computer') {
     computerScore++
+    console.log (`You loose! ${computerSelection} beats ${playerSelection}`)
 }
 
 else {
-    console.log ("It`s a tie!")
+    console.log ("It's a tie!")
 }
-
 
 console.log (`Player Score: ${playerScore}`)
 console.log (`Computer Score: ${computerScore}`)
 
-/**Computer Choice**/
-
-function getRandomChoice () {                       
-    let randomNumber = Math.floor(Math.random() * 3); /*Get 3 random numbers*/
-    switch (randomNumber) {
-        case 0:
-            return 'ROCK'
-        case 1:
-            return 'PAPER'
-        case 2:
-            return 'SCISSORS'
-    }  
 }
 
+}
+
+/*Game winner*/
+
+if (playerScore > computerScore){
+    console.log ("You won the game! Congratulations")
+}
+
+else if (playerScore < computerScore) {
+    console.log ("You lost the game! Try again.")
 }

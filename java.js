d9a8ba1c7bea6341ee56dfadd33455ed39df077b
Gui Costa <gui.costalp@gmail.com>
2022-06-06 12:40:22 +1200

    Score finished. Working on finishing game with a winner

--- java.js
+++ java.js
@@ -1,18 +1,23 @@
 
-
-for (let i = 0; i < 5; i++) {
-
-let computerSelection = getRandomChoice()
-let playerSelection = 'ROCK' /** prompt("Choose one").toUpperCase() //**.toUpperCase() used to make all strings Upper case and compere with optons | This make input case insensitive**/
 let playerScore = 0
 let computerScore = 0
 
+
+/**Loop 5x turns**/
+for (let i = 0; i < 5; i++) { 
+
+
+let computerSelection = getRandomChoice()
+let playerSelection = 'ROCK'/*prompt("Choose one").toUpperCase() //**.toUpperCase() used to make all strings Upper case and compere with optons | This make input case insensitive**/
+
+
 console.log (`Player Selection: ${playerSelection}`)
 console.log (`Computer Selection: ${computerSelection}`)
-console.log (`Round Result: ${playRound (playerSelection, computerSelection)}`)
-console.log (`Player Score: ${playerScore}`)
-console.log (`Computer Score: ${computerScore}`)
+console.log (`playRound: ${playRound (playerSelection, computerSelection)}`)
 
+
+
+/*One turn fuction (for loop runs 5 times)*/
 
 function playRound (playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
@@ -29,16 +34,35 @@ function playRound (playerSelection, computerSelection) {
 
     else {
         return 'computer'
-    }
-
-    
+    }  
     
 }
 
 
 
+/*Score fuction*/
+
+let roundResult = playRound(playerSelection, computerSelection)
+console.log (`roundResult: ${roundResult}`)
+
+if (roundResult === 'player') {
+    playerScore++
+}
+
+else if (roundResult === 'computer') {
+    computerScore++
+}
+
+else {
+    console.log ("It`s a tie!")
+}
+
+
+console.log (`Player Score: ${playerScore}`)
+console.log (`Computer Score: ${computerScore}`)
 
 /**Computer Choice**/
+
 function getRandomChoice () {                       
     let randomNumber = Math.floor(Math.random() * 3); /*Get 3 random numbers*/
     switch (randomNumber) {
@@ -52,63 +76,3 @@ function getRandomChoice () {
 }
 
 }
-
-
-
-
-
-
-
-
-
-/*Pseudo code
-    function to decide computer turn - DONE
-        can chose between paper, scissors or rock
-        var to storage result
-
-    prompt to ask users choices - DONE
-        can chose between paper, scissors or rock
-        var to storage resul
-   
-    function to set choices values
-        i.e rock wins over scissors
-        may need several functions for each possibility
-
-    function to play the round. 
-        choose between computer and user choices
-
-    string with turn result
-        "You Lose! Paper beats Rock"
-
-    Make user choice case-insensitive (rock, ROCK, RocK)
-
-    
-
-
-
-
-
-
-function playerSelection() {
-
-}
-
-function computerSelection() {
-
-}
-
-
-
-
-function playRound(playerSelection, computerSelection) {
-    // your code here!
-  }
-
-function computerPlay() {
-
-}
-  
-  
-  const computerSelection = computerPlay();
-  console.log(playRound(playerSelection, computerSelection));
-*/
